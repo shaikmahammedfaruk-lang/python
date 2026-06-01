a0c4f3c8b975eb6a7557dac386bd3ad847b0e05b
shaikmahammedfaruk-lang <shaikmahammedfaruk@gmail.com>
2026-06-01 20:25:03 +0530

    first commit

---
+++ 25-06-2026/program20.py
@@ -0,0 +1,16 @@
+lower = int(input("Enter the lower limit of the interval: "))
+upper = int(input("Enter the upper limit of the interval: "))
+
+for num in range(lower, upper + 1):
+
+    order = len(str(num))
+    temp_num = num
+    total = 0
+
+    while temp_num > 0:
+        digit = temp_num % 10
+        total += digit ** order
+        temp_num //= 10
+
+    if num == total:
+        print(num)
---
+++ 26-06-2026/program32.py
@@ -0,0 +1,3 @@
+arr = [1,2,3]
+ans = sum(arr)
+print('Sum of the array is ', ans)
